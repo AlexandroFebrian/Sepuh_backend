64f7b562ff6417c2c4bef8a032a73d377b063fe6
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-27 10:00:49 +0700

    update init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
+
+const indexRouter = require('./src/routes/index.routes');
+
+app.use("/api", indexRouter);
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+});
