424f9e93118c8b2f37303d1217269e180624c1e3
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-11-21 17:12:53 +0700

    Login sudah bisa

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -14,7 +14,7 @@ const registerUser = async (req, res) => {
         });
     }
 
-    const user = await User.findOne({ email: email});
+    const user = await User.findOne({ email: email });
     if (user) {
         return res.status(400).json({
             message: `Email user sudah terpakai!`
@@ -22,7 +22,7 @@ const registerUser = async (req, res) => {
     }
 
     try {
-        const token = jwt.sign({ email: email }, env("SECRET_KEY"), { expiresIn: "1h" });
+        const token = jwt.sign({ email: email }, env("SECRET_KEY"), { expiresIn: "3h" });
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -35,7 +35,7 @@ const registerUser = async (req, res) => {
             from: env("EMAIL_ADDRESS"),
             to: email,
             subject: 'Verify your Sepuh registration account',
-            text: `Click link below to verify your account <a href="http://${ env("HOST") }/api/users/verify/${ token }"><a>`
+            text: `Click link below to verify your account http://${ env("HOST") }/api/users/verify/${ token }`
         };
         
         transporter.sendMail(mailOptions, (error, info) => {
@@ -61,7 +61,15 @@ const registerUser = async (req, res) => {
         });
 
         return res.status(200).json({
-            ...result,
+            name: result._doc.name,
+            email: result._doc.email,
+            role: result._doc.role,
+            balance: result._doc.balance,
+            rating: result._doc.rating,
+            employees: result._doc.employees,
+            history: result._doc.history,
+            list: result._doc.list,
+            status: result._doc.status,
             verify_link: `http://${ env("HOST") }/api/users/verify/${ token }`
         });
     } catch (err) {
@@ -88,7 +96,52 @@ const verifyUser = async (req, res) => {
 }
  
 const loginUser = async (req, res) => {
-    
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: `Input tidak boleh kosong!`
+        });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(404).json({
+            message: `Email user tidak terdaftar!`
+        });
+    }
+
+    try {
+        const resultPassword = bcrypt.compareSync(password, user.password);
+        if (!resultPassword) {
+            return res.status(400).json({
+                message: `Password tidak sesuai!`
+            });
+        } else if (!user.status) {
+            return res.status(400).json({
+                message: `User belum melakukan verifikasi email!`
+            });
+        }
+
+        const token = jwt.sign({ email: email, name: user.name, role: user.role }, env("SECRET_KEY"), { expiresIn: "3h" });
+
+        return res.status(200).json({
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            balance: user.balance,
+            rating: user.rating,
+            employees: user.employees,
+            history: user.history,
+            list: user.list,
+            status: user.status,
+            token: token
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
 }
 
 const fetchUser = async (req, res) => {
