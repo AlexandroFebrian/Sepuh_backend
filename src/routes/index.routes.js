1dad927342ad0856584a6b1a2a00585118f95546
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-11-24 03:58:19 +0700

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const env = require("../config/env.config");
 
 const usersRouter = require('./users');
 const postsRouter = require('./posts');
 
 router.use("/users", usersRouter);
 router.use("/posts", postsRouter);
+router.post("/cekToken", (req, res) => {
+    const { token } = req.body;
+  
+    try {
+        const decoded = jwt.verify(token, env("SECRET_KEY"));
+        return res.status(200).json({
+            status: true,
+            data: decoded
+        })
+    } catch (error) {
+        return res.status(401).json({
+            status: false,
+            message: "Invalid token"
+        })
+    }
+})
  
 module.exports = router;
